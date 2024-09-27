0853d36b22406c698925fadf4d2f0692babd4081
mmedl94 <matthias.medl@chello.at>
2024-09-27 10:54:29 +0200

    added data.R

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' Austrian Vacation Activities
+#'
+#'The Austrian Vacation Activities dataset comprises responses from 2,961 adult
+#'tourists who spent their holiday in Austria during the 1997/98 season. The
+#'responses are coded in binary with 0 indicating that the tourist didn't
+#'partake in the activity and 1 indicating they did.
+#'
+#'
+#' @name winterActiv
+#' @aliases winterActiv
+#' @docType data
+#' @format A 2961 x 27 array of binary responses
+#' @keywords datasets
+#' @examples
+#'
+#' head(winterActiv)
+"winterActiv"
+
+#' Australian Vacation Activities
+#'
+#' The Australian Vacation Activities dataset includes responses from 1,003
+#' adult Australians who were surveyed on their vacation activities
+#' through a permission-based internet panel in 2007.The responses are coded in
+#' binary with 0 indicating that the tourist didn't partake in the activity and
+#' 1 indicating they did.
+#'
+#' @name ausActiv
+#' @aliases ausActiv
+#' @docType data
+#' @format A 1003 x 44 array of binary responses
+#' @keywords datasets
+#' @examples
+#'
+#' head(ausActiv)
+"ausActiv"
