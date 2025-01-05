45f33ece04c61e6d5504c3abfe8a5e43a335930c
mmedl94 <matthias.medl@chello.at>
2025-01-05 18:12:29 +0100

    forced the numpy installation to be of version 2 or later to ensure compatability of saves

--- R/init_env.R
+++ R/init_env.R
@@ -12,19 +12,20 @@
 #' @return -
 #' @export
 #'
-#' @examples init_env(env_name="r-lionfish", virtual_env = "virtual_env")
+#' @examples init_env(env_name = "r-lionfish", virtual_env = "virtual_env")
 #'
-init_env <- function(env_name="r-lionfish", virtual_env = "virtual_env", local=FALSE){
-
+init_env <- function(env_name = "r-lionfish", virtual_env = "virtual_env", local = FALSE) {
   # Check if python is available
   reticulate::py_available(initialize = FALSE)
 
-  required_packages <- c("pandas", "numpy", "matplotlib",
-                         "customtkinter", "statsmodels", "seaborn")
+  required_packages <- c(
+    "pandas", "numpy>=2.0", "matplotlib",
+    "customtkinter", "statsmodels", "seaborn"
+  )
 
-  if (virtual_env == "anaconda"){
+  if (virtual_env == "anaconda") {
     # check if python environment exists and create new one if not
-    if (env_name %in% reticulate::conda_list()$name==FALSE){
+    if (env_name %in% reticulate::conda_list()$name == FALSE) {
       reticulate::conda_create(env_name)
     }
     # initialize python environment
@@ -32,19 +33,19 @@ init_env <- function(env_name="r-lionfish", virtual_env = "virtual_env", local=F
 
     # check if required packages are installed and install them if not
     package_names <- reticulate::py_list_packages(envname = env_name, "conda")
-    for (package in required_packages){
-      if (package %in% package_names$package==FALSE){
-        if (package == "customtkinter"){
-          reticulate::conda_install(env_name, package, pip=TRUE)
+    for (package in required_packages) {
+      if (package %in% package_names$package == FALSE) {
+        if (package == "customtkinter") {
+          reticulate::conda_install(env_name, package, pip = TRUE)
         } else {
           reticulate::conda_install(env_name, package)
         }
       }
     }
-  } else if (virtual_env == "virtual_env"){
-    if (env_name %in% reticulate::virtualenv_list()==FALSE){
-      py_version <- unlist(reticulate::virtualenv_starter(all=TRUE)$version[1])
-      py_version = paste(py_version, collapse = ".")
+  } else if (virtual_env == "virtual_env") {
+    if (env_name %in% reticulate::virtualenv_list() == FALSE) {
+      py_version <- unlist(reticulate::virtualenv_starter(all = TRUE)$version[1])
+      py_version <- paste(py_version, collapse = ".")
       reticulate::install_python(version = py_version)
       reticulate::virtualenv_create(env_name)
     }
@@ -53,9 +54,9 @@ init_env <- function(env_name="r-lionfish", virtual_env = "virtual_env", local=F
 
     # check if required packages are installed and install them if not
     package_names <- reticulate::py_list_packages(envname = env_name, "virtualenv")
-    for (package in required_packages){
-      if (package %in% package_names$package==FALSE){
-        if (package == "customtkinter"){
+    for (package in required_packages) {
+      if (package %in% package_names$package == FALSE) {
+        if (package == "customtkinter") {
           reticulate::virtualenv_install(env_name, package)
         } else {
           reticulate::virtualenv_install(env_name, package)
@@ -67,18 +68,18 @@ init_env <- function(env_name="r-lionfish", virtual_env = "virtual_env", local=F
   base::cat(base::sprintf('Python environment "%s" successfully loaded', env_name), "\n")
 
   # Check accessibility of python functions
-  lionfish_dir <- find.package("lionfish", lib.loc=NULL, quiet = TRUE)
+  lionfish_dir <- find.package("lionfish", lib.loc = NULL, quiet = TRUE)
 
-  if (dir.exists(file.path(lionfish_dir, "/inst"))){
-    check_dir <- base::paste(lionfish_dir,"/inst/python/check_backend.py", sep = "")
+  if (dir.exists(file.path(lionfish_dir, "/inst"))) {
+    check_dir <- base::paste(lionfish_dir, "/inst/python/check_backend.py", sep = "")
   } else {
-    check_dir <- base::paste(lionfish_dir,"/python/check_backend.py", sep = "")
+    check_dir <- base::paste(lionfish_dir, "/python/check_backend.py", sep = "")
   }
   reticulate::source_python(check_dir)
   reticulate::py$check_backend()
 
   # set directory of tcl/tk installation for windows users
-  if (.Platform$OS.type == "windows"){
+  if (.Platform$OS.type == "windows") {
     home_dir <- gsub("\\\\", "/", Sys.getenv("USERPROFILE"))
 
     sys <- reticulate::import("sys")
@@ -89,8 +90,8 @@ init_env <- function(env_name="r-lionfish", virtual_env = "virtual_env", local=F
     tk <- "/tk/tk8.6"
     tcl <- "/tcl/tcl8.6"
 
-    tcl_dir <- base::paste0(home_dir,py_dir, py_version, tcl)
-    tk_dir <-  base::paste0(home_dir,py_dir, py_version, tk)
+    tcl_dir <- base::paste0(home_dir, py_dir, py_version, tcl)
+    tk_dir <- base::paste0(home_dir, py_dir, py_version, tk)
 
     Sys.setenv(TCL_LIBRARY = tcl_dir)
     Sys.setenv(TK_LIBRARY = tk_dir)
--- inst/python/heatmap.py
+++ inst/python/heatmap.py
@@ -49,10 +49,16 @@ def launch_heatmap(parent, plot_object, subplot_idx):
     if parent.initial_loop == False:
         parent.axs[subplot_idx].collections[-1].colorbar.remove()
 
+    if parent.color_scale == "default":
+        cmap = sns.cm.rocket
+    else:
+        cmap = parent.color_scale
+
     sns.heatmap(data=heatmap_data,
                 ax=parent.axs[subplot_idx],
                 yticklabels=y_tick_labels,
-                xticklabels=x_tick_labels)
+                xticklabels=x_tick_labels,
+                cmap=cmap)
     parent.axs[subplot_idx].set_xticks(parent.axs[subplot_idx].get_xticks(),
                                        parent.axs[subplot_idx].get_xticklabels(),
                                        rotation=25,
